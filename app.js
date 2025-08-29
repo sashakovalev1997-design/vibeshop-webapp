671d02b9b68c76be87d9f19b2bde99f801731361
sashakovalev1997-design <sashakovalev1997@gmail.com>
2025-08-30 00:09:55 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -68,3 +68,18 @@ sendOrderBtn.addEventListener('click', () => {
     cart.classList.remove('show');
     alert('✅ Ваш заказ отправлен!');
 });
+
+// --- Контакты (модальное окно) ---
+const contactBtn = document.getElementById('contact-btn');
+const contactModal = document.getElementById('contact-modal');
+const closeModal = document.getElementById('close-modal');
+
+contactBtn.addEventListener('click', () => {
+    contactModal.style.display = "block";
+});
+closeModal.addEventListener('click', () => {
+    contactModal.style.display = "none";
+});
+window.addEventListener('click', (e) => {
+    if (e.target === contactModal) contactModal.style.display = "none";
+});
